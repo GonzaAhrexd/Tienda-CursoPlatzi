76126347566c6c47f04b69dea66311ba893c7166
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2024-03-28 16:14:10 -0300

    Contexto global clase 9

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,4 +1,5 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom'
+import { ShoppingCartProvider } from '../../Context'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrders from '../MyOrders'
