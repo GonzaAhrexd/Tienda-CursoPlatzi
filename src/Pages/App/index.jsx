384edcf8dbb0d9b8457f743104c58774a03a9ff2
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2024-03-28 11:02:30 -0300

    Utilizando React Router Clase 4

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,3 +1,4 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrders from '../MyOrders'
@@ -6,18 +7,27 @@ import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import '../../App.css'
 
-function App() {
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/sign-in', element: <SignIn /> },
+    { path: '*', element: <NotFound /> },
+  ])
+  return routes
+}
+
+const App = () => {
 
   return (
-    <>
-    <Home/>
-    <MyAccount/>
-    <MyOrders/>
-    <MyOrder/>
-    <NotFound/>
-    <SignIn/>
     
-     </>
+   <BrowserRouter>
+   <AppRoutes/>
+   </BrowserRouter>
+    
+   
 
   )
 }
