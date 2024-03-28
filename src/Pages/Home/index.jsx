6b492291fad9f4699753dce93ce3fc18b61cdb22
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2024-03-28 14:07:42 -0300

    Llamado de api y grid clase 8

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import Card from '../../components/Card'
 import Layout from '../../components/Layout'
 function Home() {
+
+  const [items, setItems] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.escuelajs.co/api/v1/products')
+        const data = await response.json()
+        setItems(data)
+      } catch (error) {
+        console.error(`Oh no, ocurrió un error: ${error}`);
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <Layout >
       Home
-      <Card/>
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'> 
+
+      {
+        
+        items?.map(item => (
+          <Card
+          key={item.id}
+          data={item}
+          />
+          ))
+        }
+        </div>
+    
     </Layout>
   )
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 
-function Card() {
+function Card( data) {
   return (
-    <div className='bg-white border-teal-800  cursor-pointer w-56 h-60 rounded-lg'>
-        <figure className='relative mb-2 w-full h-4/5'>
-            <span className='absolute bottom-0 left-0 bg-white/60  rounded-lg text-black text-xs m-2 px-3 py-0.5 '>Electronics</span>
-            <img className="w-full h-full object-cover rounded-lg"  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmqKBtNIqMXPO4alyKcYrNZXBI36fN-Wq3ncWFX3H1ksj7PdiuX0dQg105riRJibq_nKs"    alt="Headphone" />
-            <div className="absolute top-0 right-0 flex justify-center font-bold items-center mr-1 mt-1 bg-white w-6 h-6 rounded-full p1">+</div>
+    /*
+    ID
+    title
+    price
+    description
+    images
+    category
+    */
+ 
+
+       <div className='bg-white border-teal-800  cursor-pointer w-56 h-60 rounded-lg'>
+        
        
-        </figure>
+        {console.log(data.data)}
+       <figure className='relative mb-2 w-full h-4/5'>
+       <span className='absolute bottom-0 left-0 bg-white/60  rounded-lg text-black text-xs m-2 px-3 py-0.5 '>{data.data?.category.name}</span>
+       <img className="w-full h-full object-cover rounded-lg"  src={data.data?.images[0]}    alt={data.data.title} />
+       <div className="absolute top-0 right-0 flex justify-center font-bold items-center mr-1 mt-1 bg-white w-6 h-6 rounded-full p1">+</div>
+       
+       </figure>
+       
+       {/* <p className='flex justify-between '> */}
+       <span className='text-sm font-light'>{data.data?.title}</span>
+       <span  className='text-lg font-medium'>${data.data?.price}</span>
+       {/* </p>  */}
+       
+       
 
-        <p className='flex justify-between '>
-            <span className='text-sm font-light'>Headphones</span>
-            <span  className='text-lg font-medium'>Price</span>
-        </p>
 
-    </div>
+       
+    
+       </div>
   )
 }
 
