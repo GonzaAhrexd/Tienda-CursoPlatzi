a2383a8d80e4edc7ee9f0ba5402aed1bd6ac4e97
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-03-30 16:57:19 -0300

    Avances clase 17

--- src/components/OrderCard/index.jsx
+++ src/components/OrderCard/index.jsx
@@ -9,24 +9,35 @@ function OrderCard(props) {
 
         <div className="flex justify-between mb-2">
             <div className='flex items-center gap-2'>
-                <figure className='w-20 h-20'>
+                <figure className='w-16 h-16'>
                     <img className='w-full h-full rounded-lg object-cover' src={imageURL} alt={title} />
                 </figure>
-                <p className='text-m font-light'> {title} </p>
+
+                <div className='flex flex-col justify-center gap-2' onMouseMove={() => { setIsHovered(true) }} onMouseOut={() => { setIsHovered(false) }}>
+
+                    {isHovered ? (
+                        <>
+                            <p className='text-base font-light'>{quantity}x ${price} </p>
+                            <p className='text-base font-light text-red-400'>Total: ${price * quantity} </p>
+                        </>
+                    ) : (
+                        <>
+                            <p className='font-light text-2xl'>${price * quantity} </p>
+                            <p className='text-sm font-light'> {title} </p>
+                        </>
+                    )}
+
+
+                </div>
+                        
+                        <div className='flex flex-row'>
+                            <button>+</button>
+                            <input className='h-5 w-6 border-2 border-solid border-zinc-700	' type="number" name="" id="" value={quantity} />
+                            <button>-</button>
+                        </div>
+
             </div>
-            <div className='flex flex-col items-center justify-center gap-2' onMouseMove={() => { setIsHovered(true) }} onMouseOut = {() => {setIsHovered(false)}}>
 
-                {isHovered ? (
-                    <>
-                        <p className='text-base font-light'>{quantity}x ${price} </p>
-                        <p className='text-base font-light text-red-400'>Total: ${price * quantity} </p>
-                    </>
-                ) : (
-                    <p className='font-light text-2xl'>${price*quantity} </p>
-                )}
-
-
-            </div>
             <XMarkIcon className='h-6 w-6 text-black cursor-pointer'
                 onClick={() => { console.log("clicked") }} >
 
